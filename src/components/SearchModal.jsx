35b8866aa60430b5b8accd1eb714c65a98d207fb
rlicata34 <rlicata34@gmail.com>
2025-01-24 12:01:27 -0500

    ModalWithForm comp

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,7 +1,47 @@
+import ModalWithForm from "./ModalWithForm";
+
 import "../blocks/SearchModal.css";
 
-function SearchModal() {
-  return <div className="header">SearchModal</div>;
+function SearchModal({ onClose, isOpen, activeModal }) {
+  return (
+    <ModalWithForm
+      title="Discover Events"
+      isOpen={isOpen}
+      activeModal={activeModal}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      buttonClass={`modal__submit-button-discover ${
+        isButtonActive ? "modal__submit-button_active" : ""
+      }`}
+    >
+      <label className="form__label">
+        Artist
+        <input
+          type="text"
+          className="form__input"
+          name="artist"
+          placeholder="artist"
+        />
+      </label>
+      <label className="form__label">
+        City
+        <input
+          type="text"
+          className="form__input"
+          name="city"
+          placeholder="city"
+        />
+      </label>
+      <label className="form__label">
+        Start Date
+        <input type="date" className="form__input" name="startDate" />
+      </label>
+      <label className="form__label">
+        End Date
+        <input type="date" className="form__input" name="endDate" />
+      </label>
+    </ModalWithForm>
+  );
 }
 
 export default SearchModal;
