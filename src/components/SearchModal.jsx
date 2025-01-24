d133b4c5fb2c4e8139d8360b8a137e71e672d2e5
rlicata34 <rlicata34@gmail.com>
2025-01-24 12:13:25 -0500

    hook file for form inputs and implement in searchModal

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,8 +1,15 @@
 import ModalWithForm from "./ModalWithForm";
+import { useForm } from "../hooks/useForm";
 
 import "../blocks/SearchModal.css";
 
 function SearchModal({ onClose, isOpen, activeModal }) {
+  const { values, handleChange, setValues } = useForm({
+    artist: "",
+    city: "",
+    startDate: "",
+    endDate: "",
+  });
   return (
     <ModalWithForm
       title="Discover Events"
@@ -20,7 +27,9 @@ function SearchModal({ onClose, isOpen, activeModal }) {
           type="text"
           className="form__input"
           name="artist"
+          value={values.artist}
           placeholder="artist"
+          onChange={handleChange}
         />
       </label>
       <label className="form__label">
@@ -29,16 +38,30 @@ function SearchModal({ onClose, isOpen, activeModal }) {
           type="text"
           className="form__input"
           name="city"
+          value={values.city}
           placeholder="city"
+          onChange={handleChange}
         />
       </label>
       <label className="form__label">
         Start Date
-        <input type="date" className="form__input" name="startDate" />
+        <input
+          type="date"
+          className="form__input"
+          name="startDate"
+          value={values.startDate}
+          onChange={handleChange}
+        />
       </label>
       <label className="form__label">
         End Date
-        <input type="date" className="form__input" name="endDate" />
+        <input
+          type="date"
+          className="form__input"
+          name="endDate"
+          value={values.endDate}
+          onChange={handleChange}
+        />
       </label>
     </ModalWithForm>
   );
