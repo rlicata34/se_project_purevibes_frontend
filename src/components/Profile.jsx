6ea7f517b59ca85ab5acfa24304c3ac101b5146c
rlicata34 <rlicata34@gmail.com>
2025-01-28 11:22:12 -0500

    Styles for profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,11 +7,19 @@ import "../blocks/Profile.css";
 
 function Profile({ bookmarkedEvents, handleCardBookmark, isLoggedIn }) {
   const { currentUser } = useContext(CurrentUserContext);
+
   const savedEventsNumber = bookmarkedEvents.length;
+  const singularEvent = "event";
+  const pluralEvents = "events";
+
   return (
     <div className="profile">
       <Navigation isLoggedIn={isLoggedIn} />
-      <h2 className="profile__title">{`${currentUser.username}, you have ${savedEventsNumber} saved events`}</h2>
+      <h2 className="profile__title">{`${
+        currentUser.username
+      }, you have ${savedEventsNumber} saved ${
+        savedEventsNumber === 1 ? singularEvent : pluralEvents
+      }`}</h2>
       {bookmarkedEvents.length > 0 ? (
         <ul className="cards__list">
           {bookmarkedEvents.map((event) => {
